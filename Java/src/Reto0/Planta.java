3cfb1e9d628d246c00d97d045a31397f54b93b1b
AmaroMartinez <45819622+AmaroMartinez@users.noreply.github.com>
2019-09-12 12:21:12 +0200

    Version 3

--- Java/src/Reto0/Planta.java
+++ Java/src/Reto0/Planta.java
@@ -1,15 +1,81 @@
 package Reto0;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Planta {
+public class Planta implements Comparable<Planta>, Serializable{
 
+
+	private static final long serialVersionUID = 1L;
 	private String nPlanta;
-	private ArrayList<String> aulas;
+	private ArrayList<Aula> aulas;
 	
 	Planta(){
 		this.nPlanta = "";
-		
+		this.aulas = new ArrayList<Aula>();
 	}
+
+	public String getnPlanta() {
+		return nPlanta;
+	}
+
+	public void setnPlanta(String nPlanta) {
+		this.nPlanta = nPlanta;
+	}
+
+	public ArrayList<Aula> getAulas() {
+		return aulas;
+	}
+
+	public void setAulas(ArrayList<Aula> aulas) {
+		this.aulas = aulas;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((aulas == null) ? 0 : aulas.hashCode());
+		result = prime * result + ((nPlanta == null) ? 0 : nPlanta.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Planta other = (Planta) obj;
+		if (aulas == null) {
+			if (other.aulas != null)
+				return false;
+		} else if (!aulas.equals(other.aulas))
+			return false;
+		if (nPlanta == null) {
+			if (other.nPlanta != null)
+				return false;
+		} else if (!nPlanta.equals(other.nPlanta))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Planta [nPlanta=" + nPlanta + ", aulas=" + aulas + "]";
+	}
+
+	@Override
+	public int compareTo(Planta A) {
+		String n1,n2;
+		n1 = this.nPlanta;
+		n2 = A.nPlanta ;
+		
+		return n1.compareTo(n2);
+	}
+
+	
 
 }
