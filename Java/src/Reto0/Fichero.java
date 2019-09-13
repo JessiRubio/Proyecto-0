3d48ace640ac196f4bf0e58899a587cf1b7e5b71
AmaroMartinez <45819622+AmaroMartinez@users.noreply.github.com>
2019-09-13 10:49:40 +0200

    Version 4

---
+++ Java/src/Reto0/Fichero.java
@@ -0,0 +1,22 @@
+package Reto0;
+
+public class Fichero {
+
+	private int nA = 1;
+	private int nP = 1;
+
+	public void crearFichero() {
+
+	}
+
+	public void crearDatos() {
+		
+		do {
+			do {
+				
+			} while (nA <= 15);
+			nA = 1;
+			nP++;
+		} while (nP <= 3);
+	}
+}
