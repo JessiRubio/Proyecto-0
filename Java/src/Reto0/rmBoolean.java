38efba8afaf3a64bdeac69b09d520155fe7dbd27
AmaroMartinez <45819622+AmaroMartinez@users.noreply.github.com>
2019-09-13 08:59:52 +0200

    Version 3

--- Java/src/Reto0/rmBoolean.java
+++ Java/src/Reto0/rmBoolean.java
@@ -1,11 +1,19 @@
 package Reto0;
 
+import java.util.Random;
+
 public class rmBoolean {
 	
 	private boolean bool;
 
 	public rmBoolean() {
-		
+		Random rnd = new Random();
+		int numero = rnd.nextInt(2);
+		if (numero == 0) {
+			bool = false;
+		} else if (numero == 1) {
+			bool = true;
+		}
 	}
 
 	public boolean getBool() {
