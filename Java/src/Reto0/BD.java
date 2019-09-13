f58f35ffd511d4fa63b1f0940c30d6c7ec5bbc78
AmaroMartinez <45819622+AmaroMartinez@users.noreply.github.com>
2019-09-13 09:55:58 +0200

    Version 3

--- Java/src/Reto0/BD.java
+++ Java/src/Reto0/BD.java
@@ -2,97 +2,85 @@ package Reto0;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
-
-
-
 public class BD {
-	
+
 	private static Connection conectar;
 	private Planta p;
-	
+
 	public static void conectarBD() {
-		
-		
+
 		try {
-			 conectar = DriverManager.getConnection("jdbc:mysql://localhost/reto0"
-					+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", 
+			conectar = DriverManager.getConnection("jdbc:mysql://localhost/proyecto0"
+					+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
 					"root", "");
 			System.out.println("Base de datos conectada.");
-			
-			
+
 		} catch (SQLException e) {
 			System.out.println("Error al conectar");
 			e.printStackTrace();
 		}
-		
-	} 
-	
+
+	}
+
 	public void cargarDatosPlanta(String pselec) {
-		// TODO Auto-generated method stub
-		
-		
-		try{
+
+		try {
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/reto0"
-					+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", 
+					+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
 					"root", "");
-			Statement st = con.createStatement();	
-			
-			ResultSet rs = st.executeQuery("SELECT * FROM aula WHERE nplanta='"+ pselec +"' ");
-			
-			while (rs.next()){ 
-	 
-				p.getAulas().add(new Aula((String)rs.getObject("nAula"),(boolean)rs.getObject("EstadoAlarma"),(boolean)rs.getObject("EstadoCalefaccion")));
-				
-				}
-			
-			st.close();	
+			Statement st = con.createStatement();
+
+			ResultSet rs = st.executeQuery("SELECT * FROM aula WHERE nplanta='" + pselec + "' ");
+
+			while (rs.next()) {
+
+				p.getAulas().add(new Aula((String) rs.getObject("nAula"), (boolean) rs.getObject("EstadoAlarma"),
+						(boolean) rs.getObject("EstadoCalefaccion")));
+
+			}
+
+			st.close();
 			rs.close();
 			con.close();
-			}
-		catch (SQLException sqle){
+		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 			System.out.println("error");
 		}
 
-		
 	}
 
 	public Aula cargarDatoAula(String cod) {
-		// TODO Auto-generated method stub
 
-		
-		try{
+		try {
 			conectarBD();
-			Statement st = conectar.createStatement();	
-			
-			ResultSet rs = st.executeQuery("SELECT nAula, EstadoAlarma, EstadoCalefaccion from aula where nAula='"+cod+"'");
-			
-			ArrayList<Aula> datosAulas=new ArrayList<>();
-			
-			while (rs.next()){ 
-				
-				Aula da =new Aula();
-			
+			Statement st = conectar.createStatement();
+
+			ResultSet rs = st
+					.executeQuery("SELECT nAula, EstadoAlarma, EstadoCalefaccion from aula where nAula='" + cod + "'");
+
+			ArrayList<Aula> datosAulas = new ArrayList<>();
+
+			while (rs.next()) {
+
+				Aula da = new Aula();
+
 				da.setnAula(rs.getString("nAula"));
-				da.setEstadoAlarma(rs.getBoolean("EstadoAlarma")); 
+				da.setEstadoAlarma(rs.getBoolean("EstadoAlarma"));
 				da.setEstadoCalefaccion(rs.getBoolean("EstadoCalefaccion"));
 				datosAulas.add(da);
-				
-				}
-			
-			st.close();	
+
+			}
+
+			st.close();
 			rs.close();
 			conectar.close();
-			
-			}
-		catch (SQLException sqle){
+
+		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 			System.out.println("error");
 		}
@@ -102,53 +90,50 @@ public class BD {
 	}
 
 	public void actualizarEstado(String codAula, String opSeleccionada) {
-		
-		try{
+
+		try {
 			conectarBD();
-			Statement st = conectar.createStatement();		
-			
+			Statement st = conectar.createStatement();
+
 			ResultSet rs = st.executeQuery("SELECT * FROM aula");
 
-			int resultado;
 			boolean valorEstado;
-			
-			
-			String Ssql = "UPDATE aula SET "+opSeleccionada+"=?  WHERE "+codAula+"=?";
-        
-			if (opSeleccionada.equals("true")) {
-				valorEstado=false;
+
+			if (opSeleccionada.equals("Alarma")) {
+				if ((boolean) rs.getObject("EstadoAlarma")) {
+					valorEstado = false;
+					String Ssql = "UPDATE aula SET EstadoAlarma =" + valorEstado;
+					st.execute(Ssql);
+				} else if (!(boolean) rs.getObject("EstadoAlarma")) {
+					valorEstado = true;
+					String Ssql = "UPDATE aula SET EstadoAlarma =" + valorEstado;
+					st.execute(Ssql);
+				}
+			} else if (opSeleccionada.equals("Calefaccion")) {
+				if ((boolean) rs.getObject("EstadoCalefaccion")) {
+					valorEstado = false;
+					String Ssql = "UPDATE aula SET EstadoCalefaccion =" + valorEstado;
+					st.execute(Ssql);
+				} else if (!(boolean) rs.getObject("EstadoCalefaccion")) {
+					valorEstado = true;
+					String Ssql = "UPDATE aula SET EstadoCalefaccion =" + valorEstado;
+					st.execute(Ssql);
+				}
 			}
-			else {
-				valorEstado=true;
-			}
-			
-			PreparedStatement prest = conectar.prepareStatement(Ssql);
-        
-			prest.setBoolean(1, valorEstado);
-			prest.setString(2, Aula.getnAula());
-			
-        
-			resultado = prest.executeUpdate();
-			prest.close();
-			
-			
-			st.close();	
+
+			st.close();
 			rs.close();
 			conectar.close();
-			}
-		catch (SQLException sqle){
+		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 			System.out.println("error");
 		}
-		
-	}
-	
-	public boolean consultarAula() {
-		// TODO Auto-generated method stub
-		return false;
-		
+
 	}
 
-	
+	public boolean consultarAula() {
+		return false;
+
+	}
 
 }
